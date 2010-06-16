616ede3ec99679bf4fa0e9ec899f1e1d6c71c084
Michael Delaurentis <mdelaurentis@macbook-2.home>
2010-06-16 15:32:02 -0400

    Add groovy-mode

--- emacs/dot-emacs.el
+++ emacs/dot-emacs.el
@@ -48,13 +48,45 @@
 ;;; interfacing with ELPA, the package archive.
 ;;; Move this code earlier if you want to reference
 ;;; packages in your .emacs.
+;(when
+;    (load
+;     (expand-file-name "~/.emacs.d/elpa/package.el"))
+;  (package-initialize))
+
+;; This fixes an issue where emacs tries to display doc for a symbol
+;; in the minibuffer but complains about slime-symbol-name-at-point
+;; not being defined.
+;(defun slime-symbol-name-at-point ()
+;  (slime-symbol-at-point))
+
+(eval-after-load "slime"
+  '(progn
+    (slime-setup)))
+
+;;; use groovy-mode when file ends in .groovy or has #!/bin/groovy at start
+(autoload 'groovy-mode "groovy-mode" "Groovy editing mode." t)
+(add-to-list 'load-path "~/src/env/emacs/groovy")
+(add-to-list 'auto-mode-alist '("\.groovy$" . groovy-mode))
+(add-to-list 'interpreter-mode-alist '("groovy" . groovy-mode))
+
+;;; This was installed by package-install.el.
+;;; This provides support for the package system and
+;;; interfacing with ELPA, the package archive.
+;;; Move this code earlier if you want to reference
+;;; packages in your .emacs.
 (when
     (load
      (expand-file-name "~/.emacs.d/elpa/package.el"))
   (package-initialize))
 
-;; This fixes an issue where emacs tries to display doc for a symbol
-;; in the minibuffer but complains about slime-symbol-name-at-point
-;; not being defined.
-(defun slime-symbol-name-at-point ()
-  (slime-symbol-at-point))
+
+;;; This was installed by package-install.el.
+;;; This provides support for the package system and
+;;; interfacing with ELPA, the package archive.
+;;; Move this code earlier if you want to reference
+;;; packages in your .emacs.
+(when
+    (load
+     (expand-file-name "~/.emacs.d/elpa/package.el"))
+  (package-initialize))
+
