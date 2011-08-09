46d1a789bef5229f5e9a4967242f6575ee03a2c1
Mike DeLaurentis <delaurentis@gmail.com>
2011-08-09 11:46:57 -0400

    Clean up some clojure mode stuff

--- emacs/dot-emacs.el
+++ emacs/dot-emacs.el
@@ -1,35 +1,22 @@
 ;; Setup load path.
-(add-to-list 'load-path "/Users/mdelaurentis/.emacs.d")
-(add-to-list 'load-path "/Users/mdelaurentis/src/clojure-mode")
-(add-to-list 'load-path "/Users/mdelaurentis/src/env/emacs/")
-(add-to-list 'load-path "/Users/mdelaurentis/src/swank-clojure")
-(add-to-list 'load-path "~/src/env/emacs/groovy")
-
-;;; This was installed by package-install.el.
-;;; This provides support for the package system and
-;;; interfacing with ELPA, the package archive.
-;;; Move this code earlier if you want to reference
-;;; packages in your .emacs.
-(when
-    (load
-     (expand-file-name "~/.emacs.d/elpa/package.el"))
-  (package-initialize))
-
 ;; I use clojure-mode and swank-clojure in my ~/src dir.
+(add-to-list 'load-path "/Users/mdelaurentis/.emacs.d")
+(add-to-list 'load-path "/Users/mdelaurentis/.emacs.d/slime")
+(add-to-list 'load-path "/Users/mdelaurentis/.emacs.d/clojure-mode")
+(add-to-list 'load-path "/Users/mdelaurentis/src/env/emacs/")
+(add-to-list 'load-path "~/src/env/emacs/groovy")
 
 ;; Use spaces, not tabs
 (setq-default indent-tabs-mode nil)
+
+(require 'clojure-mode)
+(require 'paredit)
 
 ;; Turn on paredit mode for .clj and .el files.
 (defun  lisp-enable-paredit-hook () (paredit-mode 1))
 (add-hook 'clojure-mode-hook 'lisp-enable-paredit-hook)
 (add-hook 'emacs-lisp-mode-hook 'lisp-enable-paredit-hook)
 
-(add-to-list 'auto-mode-alist '("\.gsp$" . nxml-mode))
-
-(require 'clojure-mode)
-
-(require 'swank-clojure)
 (autoload 'clojure-test-mode "clojure-test-mode" "Clojure test mode" t)
 (autoload 'clojure-test-maybe-enable "clojure-test-mode" "" t)
 (add-hook 'clojure-mode-hook 'clojure-test-maybe-enable)
@@ -40,22 +27,19 @@
 (add-to-list 'interpreter-mode-alist '("groovy" . groovy-mode))
 
 (setq c-default-style
-      '((groovy-mode . "java")))
+      '((groovy-mode . "java")
+        (java-mode   . "java")))
 
 (add-to-list 'auto-mode-alist '("\\.gsp$" . html-mode))
 
-;; Processing
 
-;(add-to-list 'load-path "/Users/mdelaurentis/src/processing-emacs/")
-;(autoload 'processing-mode "processing-mode" "Processing mode" t)
-;(add-to-list 'auto-mode-alist '("\\.pde$" . processing-mode))
-;; processing-mode will append /java/bin/java to this, so I had to
-;; symlink the java binary to a spot where it could be in a java/bin
-;; directory.
-;(setq processing-location "/Users/mdelaurentis/local")
+(add-to-list 'load-path "~/.emacs.d/vendor/coffee-mode")
+(require 'coffee-mode)
 
-;(add-to-list 'load-path "~/.emacs.d/vendor/coffee-mode")
-;(require 'coffee-mode)
+(require 'column-marker)
 
+(add-hook 'java-mode-hook (lambda () (interactive) (column-marker-1 80)))
+(add-hook 'groovy-mode-hook (lambda () (interactive) (column-marker-1 80)))
+(add-hook 'python-mode-hook (lambda () (interactive) (column-marker-1 80)))
 
-
+(set-face-foreground 'minibuffer-prompt "white")
