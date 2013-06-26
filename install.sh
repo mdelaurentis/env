4abfd47c5c2dd27ff6e9d59191e2986a7524bc24
Mike DeLaurentis <mdelaurentis@mdelaurentis.(none)>
2013-06-26 10:43:27 -0400

    Change install to clone repo

--- install.sh
+++ install.sh
@@ -3,6 +3,10 @@ sudo apt-get install git emacs screen make
 sudo apt-get install git python-numpy python-scipy python-matplotlib python-h5py redis-server python-pip
 sudo pip install virtualenv
 
+mkdir -p ~/src
+cd ~/src
+git clone git@github.com:mdelaurentis/env.git
+
 SCRIPT=`readlink -f $0`
 SRC_PREFIX=`dirname $SCRIPT`
 
